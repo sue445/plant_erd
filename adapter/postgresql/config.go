7e000e954378e20a5419e466c60ac76e38147bf0
sue445 <sue445@sue445.net>
2019-12-08 16:44:35 +0900

    Impl FormatDSN

---
+++ adapter/postgresql/config.go
@@ -0,0 +1,52 @@
+package postgresql
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// Config represents configuration for PostgreSQL connection
+type Config struct {
+	DBName   string
+	User     string
+	Password string
+	Host     string
+	Port     int
+}
+
+// FormatDSN formats the given Config into a DSN string which can be passed to the driver.
+func (c Config) FormatDSN() string {
+	var params []string
+
+	if len(c.DBName) > 0 {
+		params = append(params, "dbname="+c.escape(c.DBName))
+	}
+
+	if len(c.User) > 0 {
+		params = append(params, "user="+c.escape(c.User))
+	}
+
+	if len(c.Password) > 0 {
+		params = append(params, "password="+c.escape(c.Password))
+	}
+
+	if len(c.Host) > 0 {
+		params = append(params, "host="+c.escape(c.Host))
+	}
+
+	if c.Port > 0 {
+		params = append(params, "port="+strconv.Itoa(c.Port))
+	}
+
+	return strings.Join(params, " ")
+}
+
+func (c *Config) escape(str string) string {
+	if !strings.Contains(str, " ") {
+		return str
+	}
+
+	str = strings.ReplaceAll(str, "'", "\\'")
+	return fmt.Sprintf("'%s'", str)
+}
