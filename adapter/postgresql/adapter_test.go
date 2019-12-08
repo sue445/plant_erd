337120697a54257518af2216311a291a04c103ef
sue445 <sue445@sue445.net>
2019-12-08 23:54:35 +0900

    Impl GetTable

--- adapter/postgresql/adapter_test.go
+++ adapter/postgresql/adapter_test.go
@@ -2,6 +2,7 @@ package postgresql
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/sue445/plant_erd/db"
 	"os"
 	"strconv"
 	"testing"
@@ -42,7 +43,7 @@ func TestAdapter_GetAllTableNames(t *testing.T) {
 	withDatabase(func(a *Adapter) {
 		a.db.MustExec(`
 			CREATE TABLE users (
-				id   int not null primary key, 
+				id   integer not null primary key, 
 				name varchar(191)
 		);`)
 		defer func() {
@@ -51,8 +52,8 @@ func TestAdapter_GetAllTableNames(t *testing.T) {
 
 		a.db.MustExec(`
 			CREATE TABLE articles (
-				id      int not null primary key, 
-				user_id int not null, 
+				id      integer not null primary key, 
+				user_id integer not null, 
 				FOREIGN KEY (user_id) REFERENCES users(id)
 		);`)
 		defer func() {
@@ -63,6 +64,166 @@ func TestAdapter_GetAllTableNames(t *testing.T) {
 
 		if assert.NoError(t, err) {
 			assert.Equal(t, []string{"articles", "users"}, tables)
+		}
+	})
+}
+
+func TestAdapter_GetTable(t *testing.T) {
+	withDatabase(func(a *Adapter) {
+		a.db.MustExec(`
+			CREATE TABLE users (
+				id   integer not null primary key,
+				name text
+		);`)
+
+		a.db.MustExec(`
+			CREATE TABLE articles (
+				id      integer not null primary key, 
+				user_id integer not null, 
+				FOREIGN KEY(user_id) REFERENCES users(id)
+		);`)
+		a.db.MustExec("CREATE INDEX index_user_id_on_articles ON articles(user_id)")
+
+		a.db.MustExec(`
+			CREATE TABLE followers (
+				id             integer not null primary key,
+				user_id        integer not null,
+				target_user_id integer not null,
+				FOREIGN KEY(user_id)        REFERENCES users(id),
+				FOREIGN KEY(target_user_id) REFERENCES users(id)
+		);`)
+		a.db.MustExec("CREATE UNIQUE INDEX index_user_id_and_target_user_id_on_followers ON followers(user_id, target_user_id)")
+		a.db.MustExec("CREATE UNIQUE INDEX index_target_user_id_and_user_id_on_followers ON followers(target_user_id, user_id)")
+
+		type args struct {
+			tableName string
+		}
+		tests := []struct {
+			name string
+			args args
+			want *db.Table
+		}{
+			{
+				name: "users",
+				args: args{
+					tableName: "users",
+				},
+				want: &db.Table{
+					Name: "users",
+					Columns: []*db.Column{
+						{
+							Name:       "id",
+							Type:       "integer",
+							NotNull:    true,
+							PrimaryKey: true,
+						},
+						{
+							Name: "name",
+							Type: "text",
+						},
+					},
+				},
+			},
+			{
+				name: "articles",
+				args: args{
+					tableName: "articles",
+				},
+				want: &db.Table{
+					Name: "articles",
+					Columns: []*db.Column{
+						{
+							Name:       "id",
+							Type:       "integer",
+							NotNull:    true,
+							PrimaryKey: true,
+						},
+						{
+							Name:    "user_id",
+							Type:    "integer",
+							NotNull: true,
+						},
+					},
+					ForeignKeys: []*db.ForeignKey{
+						{
+							Sequence:   0,
+							FromColumn: "user_id",
+							ToTable:    "users",
+							ToColumn:   "id",
+						},
+					},
+					Indexes: []*db.Index{
+						{
+							Name:    "index_user_id_on_articles",
+							Columns: []string{"user_id"},
+							Unique:  false,
+						},
+					},
+				},
+			},
+			{
+				name: "followers",
+				args: args{
+					tableName: "followers",
+				},
+				want: &db.Table{
+					Name: "followers",
+					Columns: []*db.Column{
+						{
+							Name:       "id",
+							Type:       "integer",
+							NotNull:    true,
+							PrimaryKey: true,
+						},
+						{
+							Name:    "user_id",
+							Type:    "integer",
+							NotNull: true,
+						},
+						{
+							Name:    "target_user_id",
+							Type:    "integer",
+							NotNull: true,
+						},
+					},
+					ForeignKeys: []*db.ForeignKey{
+						{
+							Sequence:   0,
+							FromColumn: "target_user_id",
+							ToTable:    "users",
+							ToColumn:   "id",
+						},
+						{
+							Sequence:   0,
+							FromColumn: "user_id",
+							ToTable:    "users",
+							ToColumn:   "id",
+						},
+					},
+					Indexes: []*db.Index{
+						{
+							Name:    "index_target_user_id_and_user_id_on_followers",
+							Columns: []string{"target_user_id", "user_id"},
+							Unique:  true,
+						},
+						{
+							Name:    "index_user_id_and_target_user_id_on_followers",
+							Columns: []string{"user_id", "target_user_id"},
+							Unique:  true,
+						},
+					},
+				},
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, err := a.GetTable(tt.args.tableName)
+
+				if assert.NoError(t, err) {
+					assert.Equal(t, tt.want, got)
+				}
+			})
 		}
 	})
 }
--- adapter/postgresql/row.go
+++ adapter/postgresql/row.go
@@ -1,5 +1,51 @@
 package postgresql
 
+import (
+	"strconv"
+	"strings"
+)
+
 type pgStatUserTables struct {
 	Relname string `db:"relname"`
+}
+
+type informationSchemaColumns struct {
+	ColumnName string `db:"column_name"`
+	DataType   string `db:"data_type"`
+	IsNullable string `db:"is_nullable"`
+}
+
+type primaryKeys struct {
+	ColumnName string `db:"column_name"`
+}
+
+type foreignKey struct {
+	ToTable    string `db:"to_table"`
+	Column     string `db:"column"`
+	PrimaryKey string `db:"primary_key"`
+	Name       string `db:"name"`
+}
+
+type indexes struct {
+	Relname     string `db:"relname"`
+	Indisunique bool   `db:"indisunique"`
+	Indkey      string `db:"indkey"`
+	Oid         int    `db:"oid"`
+}
+
+func (i *indexes) Indkeys() []int {
+	keys := strings.Split(i.Indkey, " ")
+
+	indkeys := []int{}
+	for _, key := range keys {
+		i, _ := strconv.Atoi(key)
+		indkeys = append(indkeys, i)
+	}
+
+	return indkeys
+}
+
+type pgAttribute struct {
+	Attnum  int    `db:"attnum"`
+	Attname string `db:"attname"`
 }
