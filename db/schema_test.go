0801520b3b9a40788287c846cd301a39b61a8f88
sue445 <sue445@sue445.net>
2019-12-15 01:40:28 +0900

    Add Subset test

--- db/schema_test.go
+++ db/schema_test.go
@@ -353,3 +353,242 @@ func TestSchema_SurroundingTablesWithin(t *testing.T) {
 		})
 	}
 }
+
+func TestSchema_Subset(t *testing.T) {
+	articles := &Table{
+		Name: "articles",
+		Columns: []*Column{
+			{
+				Name:       "id",
+				Type:       "integer",
+				NotNull:    true,
+				PrimaryKey: true,
+			},
+			{
+				Name:    "user_id",
+				Type:    "integer",
+				NotNull: true,
+			},
+		},
+		ForeignKeys: []*ForeignKey{
+			{
+				FromColumn: "user_id",
+				ToTable:    "users",
+				ToColumn:   "id",
+			},
+		},
+	}
+
+	comments := &Table{
+		Name: "comments",
+		Columns: []*Column{
+			{
+				Name:       "id",
+				Type:       "integer",
+				NotNull:    true,
+				PrimaryKey: true,
+			},
+			{
+				Name:    "article_id",
+				Type:    "integer",
+				NotNull: true,
+			},
+		},
+		ForeignKeys: []*ForeignKey{
+			{
+				FromColumn: "article_id",
+				ToTable:    "articles",
+				ToColumn:   "id",
+			},
+		},
+	}
+
+	followers := &Table{
+		Name: "followers",
+		Columns: []*Column{
+			{
+				Name:       "id",
+				Type:       "integer",
+				NotNull:    true,
+				PrimaryKey: true,
+			},
+			{
+				Name:    "user_id",
+				Type:    "integer",
+				NotNull: true,
+			},
+			{
+				Name:    "target_user_id",
+				Type:    "integer",
+				NotNull: true,
+			},
+		},
+		ForeignKeys: []*ForeignKey{
+			{
+				FromColumn: "user_id",
+				ToTable:    "users",
+				ToColumn:   "id",
+			},
+			{
+				FromColumn: "target_user_id",
+				ToTable:    "users",
+				ToColumn:   "id",
+			},
+		},
+	}
+
+	followings := &Table{
+		Name: "followings",
+		Columns: []*Column{
+			{
+				Name:       "id",
+				Type:       "integer",
+				NotNull:    true,
+				PrimaryKey: true,
+			},
+			{
+				Name:    "user_id",
+				Type:    "integer",
+				NotNull: true,
+			},
+			{
+				Name:    "target_user_id",
+				Type:    "integer",
+				NotNull: true,
+			},
+		},
+		ForeignKeys: []*ForeignKey{
+			{
+				FromColumn: "user_id",
+				ToTable:    "users",
+				ToColumn:   "id",
+			},
+			{
+				FromColumn: "target_user_id",
+				ToTable:    "users",
+				ToColumn:   "id",
+			},
+		},
+	}
+
+	likes := &Table{
+		Name: "likes",
+		Columns: []*Column{
+			{
+				Name:    "article_id",
+				Type:    "integer",
+				NotNull: true,
+			},
+			{
+				Name:    "user_id",
+				Type:    "integer",
+				NotNull: true,
+			},
+		},
+		ForeignKeys: []*ForeignKey{
+			{
+				FromColumn: "article_id",
+				ToTable:    "articles",
+				ToColumn:   "id",
+			},
+			{
+				FromColumn: "user_id",
+				ToTable:    "users",
+				ToColumn:   "id",
+			},
+		},
+	}
+
+	revisions := &Table{
+		Name: "revisions",
+		Columns: []*Column{
+			{
+				Name:       "id",
+				Type:       "integer",
+				NotNull:    true,
+				PrimaryKey: true,
+			},
+			{
+				Name:    "article_id",
+				Type:    "integer",
+				NotNull: true,
+			},
+		},
+		ForeignKeys: []*ForeignKey{
+			{
+				FromColumn: "article_id",
+				ToTable:    "articles",
+				ToColumn:   "id",
+			},
+		},
+	}
+
+	users := &Table{
+		Name: "users",
+		Columns: []*Column{
+			{
+				Name:       "id",
+				Type:       "integer",
+				NotNull:    true,
+				PrimaryKey: true,
+			},
+			{
+				Name: "name",
+				Type: "text",
+			},
+		},
+	}
+
+	tables := []*Table{
+		articles,
+		comments,
+		followers,
+		followings,
+		likes,
+		revisions,
+		users,
+	}
+
+	type fields struct {
+		Tables []*Table
+	}
+	type args struct {
+		tableName string
+		distance  int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Schema
+	}{
+		{
+			name: "distance within 1 from articles",
+			fields: fields{
+				Tables: tables,
+			},
+			args: args{
+				tableName: "articles",
+				distance:  1,
+			},
+			want: &Schema{
+				Tables: []*Table{
+					articles,
+					comments,
+					likes,
+					revisions,
+					users,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Schema{
+				Tables: tt.fields.Tables,
+			}
+			got := s.Subset(tt.args.tableName, tt.args.distance)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
