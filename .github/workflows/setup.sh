fada680c24131e40d6c14623359cf14d62211d1b
sue445 <sue445@sue445.net>
2019-12-27 00:12:27 +0900

    [WIP] Setup windows build

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -35,6 +35,59 @@ case "${RUNNER_OS}" in
   sudo cp _build/macos/oci8.pc /usr/local/lib/pkgconfig/oci8.pc
   ;;
 
+"Windows")
+  if [ ! -e tmp/pacman ]; then
+    # Save only pacman files to cache
+    git clone --depth=1 --quiet https://github.com/git-for-windows/git-sdk-64
+
+    mkdir -p tmp/pacman/usr/bin
+    cp -R git-sdk-64/usr/bin/pacman* tmp/pacman/usr/bin
+
+    mkdir -p tmp/pacman/etc
+    cp -R git-sdk-64/etc/pacman.* tmp/pacman/etc
+
+    mkdir -p tmp/pacman/var/lib
+    cp -R git-sdk-64/var/lib/pacman tmp/pacman/var/lib
+
+    mkdir -p tmp/pacman/usr/share/makepkg
+    cp -R git-sdk-64/usr/share/makepkg/util* tmp/pacman/usr/share/makepkg
+
+    du -sm tmp/pacman
+  fi
+
+  # Install pacman
+  cp -R tmp/pacman/usr/bin/* /usr/bin/
+  cp -R tmp/pacman/etc/* /etc/
+  mkdir -p /var/lib/
+  cp -R tmp/pacman/var/lib/* /var/lib/
+  cp -R tmp/pacman/usr/share/makepkg/* /usr/share/makepkg/
+
+  pacman --database --check
+
+  curl -L https://raw.githubusercontent.com/git-for-windows/build-extra/master/git-for-windows-keyring/git-for-windows.gpg | pacman-key --add -
+  pacman-key --lsign-key 1A9F3986
+
+  case "${TARGET_ARCH}" in
+  "amd64")
+    pacman -S --noconfirm mingw-w64-x86_64-pkg-config
+    _build/windows/setup_oracle_x64.sh
+    ;;
+
+  "386")
+    pacman -S --noconfirm mingw-w64-i686-pkg-config
+    # _build/windows/setup_oracle_386.sh
+    ;;
+
+  *)
+    echo "Uknown TARGET_ARCH: ${TARGET_ARCH}"
+    exit 1
+  ;;
+  esac
+
+  mkdir -p /usr/local/lib/pkgconfig/
+  cp _build/windows/oci8.pc /usr/local/lib/pkgconfig/oci8.pc
+  ;;
+
 *)
   echo "Uknown OS: ${RUNNER_OS}"
   exit 1
