b90f0e13540bc8aa509aa9ce1849d328be72a1a6
sue445 <sue445@sue445.net>
2022-01-30 16:59:12 +0900

    Refactor: extract to CreateCliCommonFlags

--- cmd/plant_erd-oracle/main.go
+++ cmd/plant_erd-oracle/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/sue445/plant_erd/adapter/oracle"
+	"github.com/sue445/plant_erd/cmd"
 	"github.com/sue445/plant_erd/lib"
 	"github.com/urfave/cli"
 	"log"
@@ -26,52 +27,7 @@ func main() {
 	app.Usage = "ERD exporter with PlantUML and Mermaid format (for oracle)"
 
 	generator := lib.ErdGenerator{ShowComment: true}
-
-	commonFlags := []cli.Flag{
-		cli.StringFlag{
-			Name:        "f,file",
-			Usage:       "`FILE` for output (default: stdout)",
-			Required:    false,
-			Destination: &generator.Filepath,
-		},
-		cli.StringFlag{
-			Name:        "t,table",
-			Usage:       "Output only tables within a certain distance adjacent to each other with foreign keys from a specific `TABLE`",
-			Required:    false,
-			Destination: &generator.Table,
-		},
-		cli.IntFlag{
-			Name:        "d,distance",
-			Usage:       "Output only tables within a certain `DISTANCE` adjacent to each other with foreign keys from a specific table",
-			Required:    false,
-			Destination: &generator.Distance,
-			Value:       0,
-		},
-		cli.BoolFlag{
-			Name:        "i,skip-index",
-			Usage:       "Whether don't print index to ERD. This option is used only --format=plant_uml",
-			Required:    false,
-			Destination: &generator.SKipIndex,
-		},
-		cli.StringFlag{
-			Name:        "s,skip-table",
-			Usage:       "Skip generating table by using regex patterns",
-			Required:    false,
-			Destination: &generator.SkipTable,
-		},
-		cli.StringFlag{
-			Name:        "format",
-			Usage:       "Output format (plant_uml, mermaid. default:plant_uml)",
-			Required:    false,
-			Destination: &generator.Format,
-		},
-		cli.BoolFlag{
-			Name:        "show-comment",
-			Usage:       "Show column comment. This option is used only --format=mermaid",
-			Required:    false,
-			Destination: &generator.ShowComment,
-		},
-	}
+	commonFlags := cmd.CreateCliCommonFlags(&generator)
 
 	oracleConfig := oracle.NewConfig()
 	app.Flags = append(
--- cmd/plant_erd/main.go
+++ cmd/plant_erd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sue445/plant_erd/adapter/mysql"
 	"github.com/sue445/plant_erd/adapter/postgresql"
 	"github.com/sue445/plant_erd/adapter/sqlite3"
+	"github.com/sue445/plant_erd/cmd"
 	"github.com/sue445/plant_erd/lib"
 	"github.com/urfave/cli"
 	"log"
@@ -30,51 +31,7 @@ func main() {
 
 	generator := lib.ErdGenerator{ShowComment: true}
 
-	commonFlags := []cli.Flag{
-		cli.StringFlag{
-			Name:        "f,file",
-			Usage:       "`FILE` for output (default: stdout)",
-			Required:    false,
-			Destination: &generator.Filepath,
-		},
-		cli.StringFlag{
-			Name:        "t,table",
-			Usage:       "Output only tables within a certain distance adjacent to each other with foreign keys from a specific `TABLE`",
-			Required:    false,
-			Destination: &generator.Table,
-		},
-		cli.IntFlag{
-			Name:        "d,distance",
-			Usage:       "Output only tables within a certain `DISTANCE` adjacent to each other with foreign keys from a specific table",
-			Required:    false,
-			Destination: &generator.Distance,
-			Value:       0,
-		},
-		cli.BoolFlag{
-			Name:        "i,skip-index",
-			Usage:       "Whether don't print index to ERD. This option is used only --format=plant_uml",
-			Required:    false,
-			Destination: &generator.SKipIndex,
-		},
-		cli.StringFlag{
-			Name:        "s,skip-table",
-			Usage:       "Skip generating table by using regex patterns",
-			Required:    false,
-			Destination: &generator.SkipTable,
-		},
-		cli.StringFlag{
-			Name:        "format",
-			Usage:       "Output format (plant_uml, mermaid. default:plant_uml)",
-			Required:    false,
-			Destination: &generator.Format,
-		},
-		cli.BoolFlag{
-			Name:        "show-comment",
-			Usage:       "Show column comment. This option is used only --format=mermaid",
-			Required:    false,
-			Destination: &generator.ShowComment,
-		},
-	}
+	commonFlags := cmd.CreateCliCommonFlags(&generator)
 
 	sqlite3Database := ""
 	mysqlConfig := mysqlDriver.NewConfig()
