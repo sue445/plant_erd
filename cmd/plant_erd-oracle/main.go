3b2dc1f34ee672721e2c2c5b29f2eab5a33d6f21
sue445 <sue445@sue445.net>
2025-06-09 23:24:20 +0900

    Migrate to v3

--- cmd/plant_erd-oracle/main.go
+++ cmd/plant_erd-oracle/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/cockroachdb/errors"
 	"github.com/sue445/plant_erd/adapter/oracle"
@@ -21,78 +22,80 @@ var (
 )
 
 func main() {
-	app := cli.NewApp()
-
-	app.Version = fmt.Sprintf("%s (build. %s)", Version, Revision)
-	app.Name = "plant_erd-oracle"
-	app.Usage = "ERD exporter with PlantUML and Mermaid format (for oracle)"
-
 	generator := lib.NewErdGenerator()
 	commonFlags := cmd.CreateCliCommonFlags(generator)
 
 	oracleConfig := oracle.NewConfig()
-	app.Flags = append(
-		commonFlags,
-		&cli.StringFlag{
-			Name:        "user",
-			Usage:       "Oracle `USER`",
-			Required:    true,
-			Destination: &oracleConfig.Username,
-		},
-		&cli.StringFlag{
-			Name:        "password",
-			Usage:       "Oracle `PASSWORD`",
-			Required:    false,
-			Destination: &oracleConfig.Password,
-			EnvVars:     []string{"ORACLE_PASSWORD"},
-		},
-		&cli.StringFlag{
-			Name:        "host",
-			Usage:       "Oracle `HOST`",
-			Required:    false,
-			Destination: &oracleConfig.Host,
-			Value:       "localhost",
-		},
-		&cli.IntFlag{
-			Name:        "port",
-			Usage:       "Oracle `PORT`",
-			Required:    false,
-			Destination: &oracleConfig.Port,
-			Value:       1521,
-		},
-		&cli.StringFlag{
-			Name:        "service",
-			Usage:       "Oracle `SERVICE` name",
-			Required:    true,
-			Destination: &oracleConfig.ServiceName,
-		},
-	)
 
-	app.Action = func(_ *cli.Context) error {
-		adapter, closeDatabase, err := oracle.NewAdapter(oracleConfig)
+	command := &cli.Command{
+		Name:    "plant_erd-oracle",
+		Version: fmt.Sprintf("%s (build. %s)", Version, Revision),
+		Usage:   "ERD exporter with PlantUML and Mermaid format (for oracle)",
+		Flags: append(
+			commonFlags,
+			&cli.StringFlag{
+				Name:        "user",
+				Usage:       "Oracle `USER`",
+				Required:    true,
+				Destination: &oracleConfig.Username,
+			},
+			&cli.StringFlag{
+				Name:        "password",
+				Usage:       "Oracle `PASSWORD`",
+				Required:    false,
+				Destination: &oracleConfig.Password,
+				Sources:     cli.EnvVars("ORACLE_PASSWORD"),
+			},
+			&cli.StringFlag{
+				Name:        "host",
+				Usage:       "Oracle `HOST`",
+				Required:    false,
+				Destination: &oracleConfig.Host,
+				Value:       "localhost",
+			},
+			&cli.IntFlag{
+				Name:        "port",
+				Usage:       "Oracle `PORT`",
+				Required:    false,
+				Destination: &oracleConfig.Port,
+				Value:       1521,
+			},
+			&cli.StringFlag{
+				Name:        "service",
+				Usage:       "Oracle `SERVICE` name",
+				Required:    true,
+				Destination: &oracleConfig.ServiceName,
+			},
+		),
+		Action: func(_ context.Context, _ *cli.Command) error {
+			adapter, closeDatabase, err := oracle.NewAdapter(oracleConfig)
 
-		if err != nil {
-			return errors.WithStack(err)
-		}
+			if err != nil {
+				return errors.WithStack(err)
+			}
 
-		defer closeDatabase() //nolint:errcheck
+			defer closeDatabase() //nolint:errcheck
 
-		schema, err := lib.LoadSchema(adapter)
-		if err != nil {
-			return errors.WithStack(err)
-		}
+			schema, err := lib.LoadSchema(adapter)
+			if err != nil {
+				return errors.WithStack(err)
+			}
 
-		return generator.Run(schema) //nolint:errcheck
+			return generator.Run(schema) //nolint:errcheck
+		},
 	}
 
-	sort.Sort(cli.CommandsByName(app.Commands))
+	// Sort commands
+	sort.Slice(command.Commands, func(i, j int) bool {
+		return command.Commands[i].Name < command.Commands[j].Name
+	})
 
 	// Sort sub-command flags
-	for _, c := range app.Commands {
+	for _, c := range command.Commands {
 		sort.Sort(cli.FlagsByName(c.Flags))
 	}
 
-	err := app.Run(os.Args)
+	err := command.Run(context.Background(), os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
--- cmd/plant_erd/main.go
+++ cmd/plant_erd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/cockroachdb/errors"
 	mysqlDriver "github.com/go-sql-driver/mysql"
@@ -24,12 +25,6 @@ var (
 )
 
 func main() {
-	app := cli.NewApp()
-
-	app.Version = fmt.Sprintf("%s (build. %s)", Version, Revision)
-	app.Name = "plant_erd"
-	app.Usage = "ERD exporter with PlantUML and Mermaid format"
-
 	generator := lib.NewErdGenerator()
 	commonFlags := cmd.CreateCliCommonFlags(generator)
 
@@ -39,179 +34,187 @@ func main() {
 	mysqlPort := 0
 	postgresqlConfig := postgresql.NewConfig()
 
-	app.Commands = []*cli.Command{
-		{
-			Name:    "sqlite3",
-			Aliases: []string{"s"},
-			Usage:   "Generate ERD from sqlite3",
-			Flags: append(
-				commonFlags,
-				&cli.StringFlag{
-					Name:        "database",
-					Usage:       "SQLite3 `DATABASE` file",
-					Required:    true,
-					Destination: &sqlite3Database,
+	command := &cli.Command{
+		Name:    "plant_erd",
+		Version: fmt.Sprintf("%s (build. %s)", Version, Revision),
+		Usage:   "ERD exporter with PlantUML and Mermaid format",
+		Commands: []*cli.Command{
+			{
+				Name:    "sqlite3",
+				Aliases: []string{"s"},
+				Usage:   "Generate ERD from sqlite3",
+				Flags: append(
+					commonFlags,
+					&cli.StringFlag{
+						Name:        "database",
+						Usage:       "SQLite3 `DATABASE` file",
+						Required:    true,
+						Destination: &sqlite3Database,
+					},
+				),
+				Action: func(_ context.Context, _ *cli.Command) error {
+					adapter, closeDatabase, err := sqlite3.NewAdapter(sqlite3Database)
+
+					if err != nil {
+						return errors.WithStack(err)
+					}
+
+					defer closeDatabase() //nolint:errcheck
+
+					schema, err := lib.LoadSchema(adapter)
+					if err != nil {
+						return errors.WithStack(err)
+					}
+
+					return generator.Run(schema) //nolint:errcheck
 				},
-			),
-			Action: func(_ *cli.Context) error {
-				adapter, closeDatabase, err := sqlite3.NewAdapter(sqlite3Database)
-
-				if err != nil {
-					return errors.WithStack(err)
-				}
-
-				defer closeDatabase() //nolint:errcheck
-
-				schema, err := lib.LoadSchema(adapter)
-				if err != nil {
-					return errors.WithStack(err)
-				}
-
-				return generator.Run(schema) //nolint:errcheck
 			},
-		},
-		{
-			Name:    "mysql",
-			Aliases: []string{"m"},
-			Usage:   "Generate ERD from mysql",
-			Flags: append(
-				commonFlags,
-				&cli.StringFlag{
-					Name:        "host",
-					Usage:       "MySQL `HOST`",
-					Required:    false,
-					Destination: &mysqlHost,
-					Value:       "localhost",
-				},
-				&cli.IntFlag{
-					Name:        "port",
-					Usage:       "MySQL `PORT`",
-					Required:    false,
-					Destination: &mysqlPort,
-					Value:       3306,
-				},
-				&cli.StringFlag{
-					Name:        "user",
-					Usage:       "MySQL `USER`",
-					Required:    false,
-					Destination: &mysqlConfig.User,
-					Value:       "root",
-				},
-				&cli.StringFlag{
-					Name:        "password",
-					Usage:       "MySQL `PASSWORD`",
-					Required:    false,
-					Destination: &mysqlConfig.Passwd,
-					EnvVars:     []string{"MYSQL_PASSWORD"},
-				},
-				&cli.StringFlag{
-					Name:        "database",
-					Usage:       "MySQL `DATABASE` name",
-					Required:    true,
-					Destination: &mysqlConfig.DBName,
-				},
-				&cli.StringFlag{
-					Name:        "collation",
-					Usage:       "MySQL `COLLATION`",
-					Required:    false,
-					Destination: &mysqlConfig.Collation,
-					Value:       "utf8_general_ci",
-				},
-			),
-			Action: func(_ *cli.Context) error {
-				mysqlConfig.Net = "tcp"
-				mysqlConfig.Addr = fmt.Sprintf("%s:%d", mysqlHost, mysqlPort)
+			{
+				Name:    "mysql",
+				Aliases: []string{"m"},
+				Usage:   "Generate ERD from mysql",
+				Flags: append(
+					commonFlags,
+					&cli.StringFlag{
+						Name:        "host",
+						Usage:       "MySQL `HOST`",
+						Required:    false,
+						Destination: &mysqlHost,
+						Value:       "localhost",
+					},
+					&cli.IntFlag{
+						Name:        "port",
+						Usage:       "MySQL `PORT`",
+						Required:    false,
+						Destination: &mysqlPort,
+						Value:       3306,
+					},
+					&cli.StringFlag{
+						Name:        "user",
+						Usage:       "MySQL `USER`",
+						Required:    false,
+						Destination: &mysqlConfig.User,
+						Value:       "root",
+					},
+					&cli.StringFlag{
+						Name:        "password",
+						Usage:       "MySQL `PASSWORD`",
+						Required:    false,
+						Destination: &mysqlConfig.Passwd,
+						Sources:     cli.EnvVars("MYSQL_PASSWORD"),
+					},
+					&cli.StringFlag{
+						Name:        "database",
+						Usage:       "MySQL `DATABASE` name",
+						Required:    true,
+						Destination: &mysqlConfig.DBName,
+					},
+					&cli.StringFlag{
+						Name:        "collation",
+						Usage:       "MySQL `COLLATION`",
+						Required:    false,
+						Destination: &mysqlConfig.Collation,
+						Value:       "utf8_general_ci",
+					},
+				),
+				Action: func(_ context.Context, _ *cli.Command) error {
+					mysqlConfig.Net = "tcp"
+					mysqlConfig.Addr = fmt.Sprintf("%s:%d", mysqlHost, mysqlPort)
 
-				adapter, closeDatabase, err := mysql.NewAdapter(mysqlConfig)
+					adapter, closeDatabase, err := mysql.NewAdapter(mysqlConfig)
 
-				if err != nil {
-					return errors.WithStack(err)
-				}
+					if err != nil {
+						return errors.WithStack(err)
+					}
 
-				defer closeDatabase() //nolint:errcheck
+					defer closeDatabase() //nolint:errcheck
 
-				schema, err := lib.LoadSchema(adapter)
-				if err != nil {
-					return errors.WithStack(err)
-				}
+					schema, err := lib.LoadSchema(adapter)
+					if err != nil {
+						return errors.WithStack(err)
+					}
 
-				return generator.Run(schema) //nolint:errcheck
+					return generator.Run(schema) //nolint:errcheck
+				},
 			},
-		},
-		{
-			Name:    "postgresql",
-			Aliases: []string{"p"},
-			Usage:   "Generate ERD from PostgreSQL",
-			Flags: append(
-				commonFlags,
-				&cli.StringFlag{
-					Name:        "host",
-					Usage:       "PostgreSQL `HOST`",
-					Required:    false,
-					Destination: &postgresqlConfig.Host,
-					Value:       "localhost",
-				},
-				&cli.IntFlag{
-					Name:        "port",
-					Usage:       "PostgreSQL `PORT`",
-					Required:    false,
-					Destination: &postgresqlConfig.Port,
-					Value:       5432,
-				},
-				&cli.StringFlag{
-					Name:        "user",
-					Usage:       "PostgreSQL `USER`",
-					Required:    false,
-					Destination: &postgresqlConfig.User,
-				},
-				&cli.StringFlag{
-					Name:        "password",
-					Usage:       "PostgreSQL `PASSWORD`",
-					Required:    false,
-					Destination: &postgresqlConfig.Password,
-					EnvVars:     []string{"POSTGRES_PASSWORD"},
-				},
-				&cli.StringFlag{
-					Name:        "database",
-					Usage:       "PostgreSQL `DATABASE` name",
-					Required:    true,
-					Destination: &postgresqlConfig.DBName,
-				},
-				&cli.StringFlag{
-					Name:        "sslmode",
-					Usage:       "PostgreSQL `SSLMODE`. c.f. https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-PARAMKEYWORDS",
-					Required:    false,
-					Destination: &postgresqlConfig.SslMode,
-					Value:       "disable",
-				},
-			),
-			Action: func(_ *cli.Context) error {
-				adapter, closeDatabase, err := postgresql.NewAdapter(postgresqlConfig)
+			{
+				Name:    "postgresql",
+				Aliases: []string{"p"},
+				Usage:   "Generate ERD from PostgreSQL",
+				Flags: append(
+					commonFlags,
+					&cli.StringFlag{
+						Name:        "host",
+						Usage:       "PostgreSQL `HOST`",
+						Required:    false,
+						Destination: &postgresqlConfig.Host,
+						Value:       "localhost",
+					},
+					&cli.IntFlag{
+						Name:        "port",
+						Usage:       "PostgreSQL `PORT`",
+						Required:    false,
+						Destination: &postgresqlConfig.Port,
+						Value:       5432,
+					},
+					&cli.StringFlag{
+						Name:        "user",
+						Usage:       "PostgreSQL `USER`",
+						Required:    false,
+						Destination: &postgresqlConfig.User,
+					},
+					&cli.StringFlag{
+						Name:        "password",
+						Usage:       "PostgreSQL `PASSWORD`",
+						Required:    false,
+						Destination: &postgresqlConfig.Password,
+						Sources:     cli.EnvVars("POSTGRES_PASSWORD"),
+					},
+					&cli.StringFlag{
+						Name:        "database",
+						Usage:       "PostgreSQL `DATABASE` name",
+						Required:    true,
+						Destination: &postgresqlConfig.DBName,
+					},
+					&cli.StringFlag{
+						Name:        "sslmode",
+						Usage:       "PostgreSQL `SSLMODE`. c.f. https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-PARAMKEYWORDS",
+						Required:    false,
+						Destination: &postgresqlConfig.SslMode,
+						Value:       "disable",
+					},
+				),
+				Action: func(_ context.Context, _ *cli.Command) error {
+					adapter, closeDatabase, err := postgresql.NewAdapter(postgresqlConfig)
 
-				if err != nil {
-					return errors.WithStack(err)
-				}
+					if err != nil {
+						return errors.WithStack(err)
+					}
 
-				defer closeDatabase() //nolint:errcheck
+					defer closeDatabase() //nolint:errcheck
 
-				schema, err := lib.LoadSchema(adapter)
-				if err != nil {
-					return errors.WithStack(err)
-				}
+					schema, err := lib.LoadSchema(adapter)
+					if err != nil {
+						return errors.WithStack(err)
+					}
 
-				return generator.Run(schema) //nolint:errcheck
+					return generator.Run(schema) //nolint:errcheck
+				},
 			},
 		},
 	}
 
-	sort.Sort(cli.CommandsByName(app.Commands))
+	// Sort commands
+	sort.Slice(command.Commands, func(i, j int) bool {
+		return command.Commands[i].Name < command.Commands[j].Name
+	})
 
 	// Sort sub-command flags
-	for _, c := range app.Commands {
+	for _, c := range command.Commands {
 		sort.Sort(cli.FlagsByName(c.Flags))
 	}
 
-	err := app.Run(os.Args)
+	err := command.Run(context.Background(), os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
